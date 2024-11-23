2fa1604931d509056154b0d903ac26709ef38445
Pedro Karasudani <pedro.karasudani@hotmail.com>
2024-11-23 16:00:21 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -3,7 +3,9 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,12 +15,15 @@ public class Category implements Serializable  {
     private Long id;
     private String name;
 
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
     public Category() {
     }
 
     public Category(Long id, String name)  {
-        this.name = name;
         this.id = id;
+        this.name = name;
     }
 
     public Long getId() {
@@ -35,6 +40,10 @@ public class Category implements Serializable  {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
